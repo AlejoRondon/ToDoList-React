ad698e2e1e38b093c67919ed9904efe357ce8a66
Alejandro Rondón <blackphotografy@gmail.com>
2020-08-20 15:31:30 -0500

    5-HandlingEvents

--- src/molecules/StickyForm/StickyForm.js
+++ src/molecules/StickyForm/StickyForm.js
@@ -2,29 +2,72 @@ import React from "react";
 import "./StickyForm.css";
 
 export default class StickyForm extends React.Component {
+  handleChange = (e) => {
+    console.log({
+      name: e.target.name,
+      value: e.target.value
+    });
+  };
+  handleClick = (e) => {
+    console.log("Button was clicked");
+    console.log({
+      name: e.target.name,
+      value: e.target.value
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submiting form");
+    console.log({
+      name: e.target.name,
+      value: e.target.value
+    });
+  };
   render() {
     return (
       <div>
         <div className="card text-center w-100">
           <div className="card-header">New StickyNote</div>
           <div className="card-body">
-            <form action="#">
+            <form onSubmit={this.handleSubmit} action="#" name="newStickyForm">
               <div className="form-group">
                 <label>Email</label>
-                <input className="form-control" type="email" name="email" />
+                <input
+                  onChange={this.handleChange}
+                  className="form-control"
+                  type="email"
+                  name="email"
+                />
               </div>
 
               <div className="form-group">
                 <label>Title</label>
-                <input className="form-control" type="text" name="jobTitle" />
+                <input
+                  onChange={this.handleChange}
+                  className="form-control"
+                  type="text"
+                  name="jobTitle"
+                />
               </div>
 
               <div className="form-group">
                 <label>Description</label>
-                <input className="form-control" type="text" name="twitter" />
+                <input
+                  onChange={this.handleChange}
+                  className="form-control"
+                  type="text"
+                  name="twitter"
+                />
               </div>
 
-              <button className="btn btn-primary">Save</button>
+              <button
+                // type="submit"
+                name="submit"
+                onClick={this.handleClick}
+                className="btn btn-primary"
+              >
+                Save
+              </button>
             </form>
           </div>
           <div className="card-footer text-muted"></div>
